2ad4fce43c0ae3de20a289b5692a8105db5f5ccd
Bigthinz <bigthinzcount@gmail.com>
2021-07-02 16:44:38 -0700

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {Routes ,RouterModule} from '@angular/router'
 import { FormsModule } from '@angular/forms';
 import { LoginComponent} from './login.component';
 import { SignupComponent} from './signup.component';
@@ -7,10 +8,24 @@ import { SignupComponent} from './signup.component';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+const routes: Routes = [
+  {
+    path:'',
+    component: LoginComponent
+  },
+
+  {
+    path:'signup',
+    component: SignupComponent
+  }
+]
+
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(routes) ],
   declarations: [ AppComponent, HelloComponent, LoginComponent, SignupComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  exports:[RouterModule]
 })
 export class AppModule { 
 
